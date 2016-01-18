8fe7bd2cca740ca116d10fa1426b71d4996e1d56
bokyoungbin <bokyoungbin@gmail.com>
2016-01-18 20:27:26 +0900

    orientDB version of SSM

---
+++ src/main/java/edu/snu/mist/task/SSM/SSM.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2016 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.snu.mist.task.SSM;
+
+import org.apache.reef.wake.Identifier;
+
+/**
+ * This interface is the basic representation of the Stream State Manager.
+ * It is used by stateful operators to store states into the database.
+ * Since this is a key-value type database, the key is the Identifier, and the value is generic.
+ *
+ * TODO: We could later save other objects other than states.
+ */
+
+public interface SSM {
+
+    /**
+     * Opens the database.
+     * @return true if the open worked well, false if not.
+     */
+    boolean open();
+
+    /**
+     * Closes the database.
+     * @return true if the database was closed well, false if not.
+     */
+    boolean close();
+
+    /**
+     * Stores the key-value pair in the database.
+     * @param identifier The identifier of the operator.
+     * @param value The value will be the state that the operator needs to store.
+     * @param <I> Type of the value (state) to be stored in the database.
+     * @return true if set worked well, false if not.
+     */
+    <I> boolean set(final Identifier identifier, final I value);
+
+    /**
+     *
+     * @param identifier The identifier of the operator.
+     * @param <I> Type of the value (state) to be stored in the database.
+     * @return true if the value was grabbed, false if nothing was saved.
+     */
+    <I> I get(final Identifier identifier);
+
+    /**
+     *
+     * @param identifier The identifier of the operator.
+     * @return true if there was data to delete, false if deletion did not occur.
+     */
+    boolean delete(final Identifier identifier);
+}
---
+++ src/main/java/edu/snu/mist/task/SSM/SSMImpl.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2016 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.snu.mist.task.SSM;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import org.apache.reef.wake.Identifier;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This is the OrientDB implementation of the SSM, using a Document-store type of database.
+ * The key (document) is the identifier of the operator.
+ * There is only one field at all times - "state". All values (states) are stored under this field.
+ */
+
+public final class SSMImpl implements SSM{
+
+    private static final Logger LOG = Logger.getLogger(SSMImpl.class.getName());
+    public static ODatabaseDocumentTx db;
+
+    @Override
+    public boolean open (){
+        try {
+            //The database is opened under this url.
+            db = new ODatabaseDocumentTx("plocal:/tmp/SSM-orientdb");
+            if (db.exists()){
+                LOG.log(Level.INFO, "Opening existing DB");
+                //The id, password of the database.
+                db.open("admin", "admin");
+                db.drop();
+                db.create();
+            }
+            else {
+                LOG.log(Level.INFO, "Creating new DB");
+                db.create();
+            }
+            return true;
+        } catch (Exception e){
+            e.printStackTrace();
+            LOG.log(Level.SEVERE, "OrientDB failed to open.");
+            return false;
+        }
+    }
+
+    @Override
+    public boolean close(){
+        try {
+            db.close();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            LOG.log(Level.SEVERE, "OrientDB failed to close.");
+            return false;
+        }
+    }
+
+    @Override
+    public <I> boolean set (final Identifier identifier, final I value){
+        try {
+            for (ODocument doc: db.browseClass((identifier.toString()))){
+                //If there is an existing identifier, update the document
+                if (doc.containsField("state")){
+                    doc.field("state", value);
+                    LOG.log(Level.INFO, "saving (" + identifier+ ", "+ value+")");
+                    doc.save();
+                    return true;
+                }
+            }
+            return false;
+
+        } catch (Exception e) {
+
+            try{
+                //If there is no existing identifier, create a new document
+                //TODO: This exception handling could lead to unnecessary exception handling load.
+                final ODocument document = new ODocument(identifier.toString());
+                document.field("state", value);
+                LOG.log(Level.INFO, "saving (" + identifier+ ", "+ value+")");
+
+                document.save();
+                return true;
+            } catch (Exception f){
+                f.printStackTrace();
+                LOG.log(Level.SEVERE, "OrientDB failed to save.");
+                return false;
+            }
+        }
+    };
+
+    @Override
+    public <I> I get (final Identifier identifier){
+        try{
+            for (ODocument doc : db.browseClass(identifier.toString())){
+                if (doc!=null){
+                    return doc.field("state");
+                }
+                else{
+                    LOG.log(Level.SEVERE, "OrientDB failed to get state.");
+                    return null;
+                }
+            }
+            return null;
+
+        } catch (Exception e){
+            e.printStackTrace();
+            LOG.log(Level.SEVERE, "OrientDB failed to get state.");
+            return null;
+        }
+    };
+
+    @Override
+    public boolean delete (final Identifier identifier){
+        try{
+            for (ODocument doc : db.browseClass(identifier.toString())) {
+                LOG.log(Level.INFO, "deleting (" + identifier + ", " + doc.field("state") + ")");
+                doc.delete();
+            }
+            return true;
+
+        } catch (Exception e){
+            LOG.log(Level.SEVERE, "Nothing to delete.");
+            return false;
+        }
+    };
+
+}
