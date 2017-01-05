27742b33d79b2c163266216444c881da0582cf57
KTK <kyungissac@gmail.com>
2017-01-05 16:10:10 +0900

    [MIST-388] Implement an example SessionWindow

--- bin/run_example.sh
+++ bin/run_example.sh
@@ -36,6 +36,7 @@ case $1 in
   JoinAndApplyStateful) ;;
   KMeansClustering) ;;
   KafkaSource) ;;
+  SessionWindow) ;;
   *)
     echo "Invalid input. Here is an example for using this script."
     echo "If you want to run HelloMIST with sink source option, please type like below."
