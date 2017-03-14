51a127265af12183e1e24ab7a075f747205db020
sanha <sanhaleehana@naver.com>
2017-03-14 18:08:03 +0900

    [MIST-479] add unit test

--- src/test/java/edu/snu/mist/common/sources/MQTTSourceTest.java
+++ src/test/java/edu/snu/mist/common/sources/MQTTSourceTest.java
@@ -15,280 +15,204 @@
  */
 package edu.snu.mist.common.sources;
 
-import edu.snu.mist.common.MistDataEvent;
-import edu.snu.mist.common.MistWatermarkEvent;
 import edu.snu.mist.common.OutputEmitter;
-import edu.snu.mist.common.functions.MISTFunction;
-import edu.snu.mist.common.functions.MISTPredicate;
-import edu.snu.mist.common.functions.WatermarkTimestampFunction;
-import edu.snu.mist.common.shared.NettySharedResource;
-import edu.snu.mist.common.stream.NettyChannelHandler;
-import edu.snu.mist.common.stream.textmessage.NettyTextMessageStreamGenerator;
-import io.netty.channel.ChannelHandlerContext;
+import edu.snu.mist.common.shared.MQTTSharedResource;
+import io.moquette.server.Server;
 import junit.framework.Assert;
-import org.apache.reef.io.Tuple;
-import org.apache.reef.io.network.util.StringIdentifierFactory;
 import org.apache.reef.tang.Injector;
 import org.apache.reef.tang.Tang;
 import org.apache.reef.tang.exceptions.InjectionException;
+import org.eclipse.paho.client.mqttv3.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import static java.lang.Thread.sleep;
 
 public final class MQTTSourceTest {
 
-  private static final Logger LOG = Logger.getLogger(NettySharedResource.class.getName());
-  private static final String SERVER_ADDR = "localhost";
-  private static final int SERVER_PORT = 12112;
+  private static final String HOST = "127.0.0.1";
+  private static final String PORT = "12113";
+  private static final String BROKER_URI =
+      new StringBuilder().append("tcp://").append(HOST).append(":").append(PORT).toString();
+  private static final String FIRST_TOPIC = "testTopic1";
+  private static final String SECOND_TOPIC = "testTopic2";
+  private static final String DIR_PATH_PREFIX = "/tmp/mist/MQTTSourceTest-";
 
-  private NettySharedResource nettySharedResource;
-  private StringIdentifierFactory identifierFactory;
-  private ScheduledExecutorService scheduler;
+  private MQTTSharedResource mqttSharedResource;
+  private Server mqttBroker;
 
   @Before
-  public void setUp() throws InjectionException {
+  public void setUp() throws InjectionException, IOException {
     final Injector injector = Tang.Factory.getTang().newInjector();
-    nettySharedResource = injector.getInstance(NettySharedResource.class);
-    identifierFactory = injector.getInstance(StringIdentifierFactory.class);
-    scheduler = Executors.newScheduledThreadPool(1);
+    mqttSharedResource = injector.getInstance(MQTTSharedResource.class);
+    // create local mqtt broker
+    final Properties brokerProps = new Properties();
+    brokerProps.put("port", PORT);
+    brokerProps.put("host", HOST);
+    brokerProps.put("allow_anonymous", "true");
+    brokerProps.put("persistent_store", new StringBuilder()
+        .append(DIR_PATH_PREFIX)
+        .append((Long)System.currentTimeMillis())
+        .append(".mapdb")
+        .toString());
+    mqttBroker = new Server();
+    mqttBroker.startServer(brokerProps);
   }
 
   @After
   public void tearDown() throws Exception {
-    nettySharedResource.close();
-    scheduler.shutdown();
+    mqttSharedResource.close();
+    mqttBroker.stopServer();
   }
 
   /**
-   * Test whether the created source using DataGenerator by NettyTextDataGeneratorFactory receive event-time data
-   * correctly from netty server, and generate proper punctuated watermark and outputs.
-   * It creates 4 sources each having data generator using Netty server.
+   * Test whether the MQTTDataGenerator fetches input stream from a MQTT local broker
+   * (created through Moquette), and generates data correctly.
+   * Two DataGenerators subscribing different topic from identical broker will be tested.
    * @throws Exception
    */
   @Test(timeout = 4000L)
-  public void testPunctuatedNettyTextSource() throws Exception {
-    final int numSources = 4;
-    final int numData = 3;
-    final int numWatermark = 2;
-    final List<String> inputStreamWithTimestamp = Arrays.asList(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.:100",
-        "In in leo nec erat fringilla mattis eu non massa.:800",
-        "Watermark:1000",
-        "Cras quis diam suscipit, commodo enim id, pulvinar nunc.:1200",
-        "Watermark:1500");
-    final List<String> expectedDataWithoutTimestamp = Arrays.asList(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "In in leo nec erat fringilla mattis eu non massa.",
-        "Cras quis diam suscipit, commodo enim id, pulvinar nunc.");
-    final List<Long> expectedPunctuatedWatermark = Arrays.asList(1000L, 1500L);
-    final CountDownLatch dataCountDownLatch = new CountDownLatch(numSources * numData);
-    final CountDownLatch watermarkCountDownLatch = new CountDownLatch(numSources * numWatermark);
-    final CountDownLatch channelCountDown = new CountDownLatch(numSources);
-    LOG.log(Level.FINE, "Count down data: {0}", dataCountDownLatch);
-    LOG.log(Level.FINE, "Count down watermark: {0}", watermarkCountDownLatch);
-    // create netty server
-    try (final NettyTextMessageStreamGenerator textMessageStreamGenerator =
-             new NettyTextMessageStreamGenerator(SERVER_ADDR, SERVER_PORT,
-                 new TestChannelHandler(channelCountDown))) {
-      final Injector injector = Tang.Factory.getTang().newInjector();
+  public void testMQTTDataGenerator() throws Exception {
+    final List<String> inputStream1 = new ArrayList<>();
+    final List<String> inputStream2 = new ArrayList<>();
+    inputStream1.add("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+    inputStream1.add("In in leo nec erat fringilla mattis eu non massa.");
+    inputStream2.add("Cras quis diam suscipit, commodo enim id, pulvinar nunc.");
 
-      // source list
-      final List<Tuple<DataGenerator, EventGenerator>> sources = new LinkedList<>();
-      // result data list
-      final List<List<String>> punctuatedDataResults = new LinkedList<>();
-      // result watermark list
-      final List<List<Long>> punctuatedWatermarkResults = new LinkedList<>();
-      // Create sources having punctuated watermark
-      for (int i = 0; i < numSources; i++) {
-        final DataGenerator<String> dataGenerator =
-            new NettyTextDataGenerator(SERVER_ADDR, SERVER_PORT, nettySharedResource);
+    final CountDownLatch dataCountDownLatch1 = new CountDownLatch(inputStream1.size());
+    final CountDownLatch dataCountDownLatch2 = new CountDownLatch(inputStream2.size());
+    final List<String> result1 = new ArrayList<>();
+    final List<String> result2 = new ArrayList<>();
 
-        final MISTFunction<String, Tuple<String, Long>> extractFunc = (input) ->
-            new Tuple<>(input.toString().split(":")[0], Long.parseLong(input.toString().split(":")[1]));
-        final MISTPredicate<String> isWatermark = (input) -> input.toString().split(":")[0].equals("Watermark");
-        final WatermarkTimestampFunction<String> parseTsFunc =
-            (input) -> Long.parseLong(input.toString().split(":")[1]);
-        final EventGenerator<String> eventGenerator =
-            new PunctuatedEventGenerator<>(extractFunc, isWatermark, parseTsFunc);
-        sources.add(new Tuple<>(dataGenerator, eventGenerator));
-        dataGenerator.setEventGenerator(eventGenerator);
+    // create data generators
+    final MQTTDataGenerator dataGenerator1 = mqttSharedResource.getDataGenerator(BROKER_URI, FIRST_TOPIC);
+    final MQTTDataGenerator dataGenerator2 = mqttSharedResource.getDataGenerator(BROKER_URI, SECOND_TOPIC);
+    final SourceTestEventGenerator eventGenerator1 = new SourceTestEventGenerator(result1, dataCountDownLatch1);
+    final SourceTestEventGenerator eventGenerator2 = new SourceTestEventGenerator(result2, dataCountDownLatch2);
+    dataGenerator1.setEventGenerator(eventGenerator1);
+    dataGenerator2.setEventGenerator(eventGenerator2);
 
-        final List<String> receivedData = new LinkedList<>();
-        final List<Long> receivedWatermark = new LinkedList<>();
-        punctuatedDataResults.add(receivedData);
-        punctuatedWatermarkResults.add(receivedWatermark);
-        eventGenerator.setOutputEmitter(new SourceTestOutputEmitter<>(receivedData, receivedWatermark,
-            dataCountDownLatch, watermarkCountDownLatch));
+    dataGenerator1.start();
+    dataGenerator2.start();
+
+    // create test client and publish data
+    final PublishTestClient publishClient = new PublishTestClient(BROKER_URI);
+    publishClient.publish(inputStream1, FIRST_TOPIC);
+    publishClient.publish(inputStream2, SECOND_TOPIC);
+
+    sleep(1000); //asdasds
+
+    publishClient.stop();
+    dataGenerator1.close();
+    dataGenerator2.close();
+
+    Assert.assertEquals(inputStream1, result1);
+    Assert.assertEquals(inputStream2, result2);
+  }
+
+  /**
+   * Simple Paho MQTT publishing client for test.
+   */
+  private final class PublishTestClient implements MqttCallback {
+    /**
+     * The actual Paho MQTT client.
+     */
+    private MqttClient client;
+
+    /**
+     * Construct a client connected with target MQTT broker.
+     * @param brokerURI the URI of broker to connect
+     */
+    PublishTestClient(final String brokerURI) {
+      try {
+        this.client = new MqttClient(brokerURI, "MistTestClient");
+        client.connect();
+        client.setCallback(this);
+      } catch (final MqttException e) {
+        e.printStackTrace();
       }
-
-      // Start to receive data stream from stream generator
-      for (final Tuple<DataGenerator, EventGenerator> source : sources) {
-        source.getValue().start(); // start event generator
-        source.getKey().start(); // start data generator
-      }
-
-      // Wait until all sources connect to stream generator
-      channelCountDown.await();
-      inputStreamWithTimestamp.forEach(textMessageStreamGenerator::write);
-      // Wait until all data are sent to source
-      dataCountDownLatch.await();
-      watermarkCountDownLatch.await();
-      for (final List<String> received : punctuatedDataResults) {
-        Assert.assertEquals(expectedDataWithoutTimestamp, received);
-      }
-      for (final List<Long> received : punctuatedWatermarkResults) {
-        Assert.assertEquals(expectedPunctuatedWatermark, received);
-      }
-
-      // Closes
-      for (final Tuple<DataGenerator, EventGenerator> source : sources) {
-        source.getKey().close(); // stop data generator
-        source.getValue().close(); // stop event generator
-      }
-
     }
-  }
 
-  /**
-   * Test whether the created sources by NettyTextDataGeneratorFactory receive processing-time data
-   * correctly from netty server, and generate periodic watermark.
-   * It creates a netty source server and 4 receivers.
-   * @throws Exception
-   */
-  @Test(timeout = 4000L)
-  public void testPeriodicNettyTextSource() throws Exception {
-    final int numSources = 1;
-    final int numData = 3;
-    final int numWatermarkToWait = 5;
-    final long period = 100;
-    final long epsilon = 10;
-    final List<String> inputStream = Arrays.asList(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "In in leo nec erat fringilla mattis eu non massa.",
-        "Cras quis diam suscipit, commodo enim id, pulvinar nunc.");
-    final CountDownLatch dataCountDownLatch = new CountDownLatch(numSources * numData);
-    final CountDownLatch watermarkCountDownLatch = new CountDownLatch(numSources * numWatermarkToWait);
-    final CountDownLatch channelCountDown = new CountDownLatch(numSources);
-    LOG.log(Level.FINE, "Count down data: {0}", dataCountDownLatch);
-    LOG.log(Level.FINE, "Count down watermark: {0}", watermarkCountDownLatch);
-    // create netty server
-    try (final NettyTextMessageStreamGenerator textMessageStreamGenerator =
-             new NettyTextMessageStreamGenerator(SERVER_ADDR, SERVER_PORT,
-                 new TestChannelHandler(channelCountDown))) {
-      // Create source having periodic watermark
-      final DataGenerator<String> dataGenerator =
-          new NettyTextDataGenerator(SERVER_ADDR, SERVER_PORT, nettySharedResource);
-      final EventGenerator<String> eventGenerator =
-          new PeriodicEventGenerator<>(null, period, period, TimeUnit.MILLISECONDS, scheduler);
-      dataGenerator.setEventGenerator(eventGenerator);
-
-      final List<String> periodicReceivedData = new LinkedList<>();
-      final List<Long> periodicReceivedWatermark = new LinkedList<>();
-      eventGenerator.setOutputEmitter(new SourceTestOutputEmitter<>(periodicReceivedData,
-          periodicReceivedWatermark, dataCountDownLatch, watermarkCountDownLatch));
-
-      // Start to receive data stream from stream generator
-      eventGenerator.start();
-      dataGenerator.start();
-
-      // Wait until all sources connect to stream generator
-      channelCountDown.await();
-      inputStream.forEach(textMessageStreamGenerator::write);
-      // Wait until all data are sent to source
-      dataCountDownLatch.await();
-      watermarkCountDownLatch.await();
-
-      Assert.assertEquals(inputStream, periodicReceivedData);
-      Long lastTimestamp = 0L;
-      for (final Long timestamp : periodicReceivedWatermark) {
-        if (lastTimestamp != 0L) {
-          Assert.assertTrue(Math.abs(period - (timestamp - lastTimestamp)) < epsilon);
+    /**
+     * Publish message toward the connected broker.
+     */
+    void publish(final List<String> messageList,
+                 final String topic) {
+      try {
+        for (final String messagePayload : messageList) {
+          client.publish(topic, messagePayload.getBytes(), 1, false);
         }
-        lastTimestamp = timestamp;
+      } catch (final MqttException e) {
+        e.printStackTrace();
       }
-
-      // Closes
-      eventGenerator.close();
-      dataGenerator.close();
     }
-  }
 
-  /**
-   * A test class for channel handler.
-   */
-  final class TestChannelHandler implements NettyChannelHandler {
-    private final CountDownLatch countDownLatch;
-
-    public TestChannelHandler(final CountDownLatch countDownLatch) {
-      this.countDownLatch = countDownLatch;
+    /**
+     * Stop the connection between the target broker.
+     */
+    void stop() {
+      try {
+        client.disconnect();
+        client.close();
+      } catch (final MqttException e) {
+        e.printStackTrace();
+      }
     }
 
     @Override
-    public void channelActive(final ChannelHandlerContext ctx) throws Exception {
-      countDownLatch.countDown();
-    }
-
-    @Override
-    public void channelRead(final ChannelHandlerContext ctx, final Object msg) throws Exception {
+    public void connectionLost(final Throwable cause) {
       // do nothing
     }
 
     @Override
-    public void channelInactive(final ChannelHandlerContext ctx) throws Exception {
+    public void messageArrived(final String topic, final MqttMessage message) {
       // do nothing
     }
 
     @Override
-    public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) throws Exception {
+    public void deliveryComplete(final IMqttDeliveryToken token) {
       // do nothing
     }
   }
 
   /**
-   * Output Emitter for source test.
+   * Event Generator for source test.
+   * It stores the data which are sent from mqtt data generator.
    */
-  class SourceTestOutputEmitter<String> implements OutputEmitter {
+  private final class SourceTestEventGenerator implements EventGenerator<MqttMessage> {
     private final List<String> dataList;
-    private final List<Long> watermarkList;
     private final CountDownLatch dataCountDownLatch;
-    private final CountDownLatch watermarkCountDownLatch;
 
-    public SourceTestOutputEmitter(final List<String> dataList,
-                                   final List<Long> watermarkList,
-                                   final CountDownLatch dataCountDownLatch,
-                                   final CountDownLatch watermarkCountDownLatch) {
+    SourceTestEventGenerator(final List<String> dataList,
+                             final CountDownLatch dataCountDownLatch) {
       this.dataList = dataList;
-      this.watermarkList = watermarkList;
       this.dataCountDownLatch = dataCountDownLatch;
-      this.watermarkCountDownLatch = watermarkCountDownLatch;
     }
 
     @Override
-    public void emitData(final MistDataEvent data) {
-      dataList.add((String)data.getValue());
+    public void emitData(final MqttMessage input) {
+      dataList.add(new String(input.getPayload()));
       dataCountDownLatch.countDown();
     }
 
     @Override
-    public void emitData(final MistDataEvent data, final int index) {
-      // source test output emitter does not emit data according to the index
-      this.emitData(data);
+    public void start() {
+      // do nothing
     }
 
     @Override
-    public void emitWatermark(final MistWatermarkEvent watermark) {
-      watermarkList.add(watermark.getTimestamp());
-      watermarkCountDownLatch.countDown();
+    public void close() {
+      // do nothing
+    }
+
+    @Override
+    public void setOutputEmitter(final OutputEmitter emitter) {
+      // do nothing
     }
   }
 }
