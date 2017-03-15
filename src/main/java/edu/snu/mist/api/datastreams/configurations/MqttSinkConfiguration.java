7a87e0d517c1040fc51d0195851f66155e808c69
Taegeon Um <taegeonum@gmail.com>
2017-03-15 16:47:32 +0900

    [MIST-485] Implement MQTT sink API (#491)
    This PR addressed #485 by 
    * creating `mqttSink(brokerAddr, topic)` API 
    
    Closes #485

---
+++ src/main/java/edu/snu/mist/api/datastreams/configurations/MqttSinkConfiguration.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2017 Seoul National University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.snu.mist.api.datastreams.configurations;
+
+import edu.snu.mist.common.parameters.MQTTBrokerAddress;
+import edu.snu.mist.common.parameters.MQTTTopic;
+import org.apache.reef.tang.formats.ConfigurationModule;
+import org.apache.reef.tang.formats.ConfigurationModuleBuilder;
+import org.apache.reef.tang.formats.RequiredParameter;
+
+/**
+ * The class represents the mqtt socket sink configuration.
+ */
+public final class MqttSinkConfiguration extends ConfigurationModuleBuilder {
+  /**
+   * The parameter for MQTT broker URI.
+   */
+  public static final RequiredParameter<String> MQTT_BROKER_URI = new RequiredParameter<>();
+
+  /**
+   * The parameter for MQTT topic name.
+   */
+  public static final RequiredParameter<String> MQTT_TOPIC = new RequiredParameter<>();
+
+  public static final ConfigurationModule CONF = new MqttSinkConfiguration()
+      .bindNamedParameter(MQTTBrokerAddress.class, MQTT_BROKER_URI)
+      .bindNamedParameter(MQTTTopic.class, MQTT_TOPIC)
+      // TODO[MIST-487]: .bindImplementation(Sink.class, MqttSink.class)
+      .build();
+}
