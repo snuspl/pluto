9467381e7b63d6f80091612440a504180b0f1faf
DifferentSC <bert1234@snu.ac.kr>
2017-04-11 15:43:25 +0900

    [MIST-554] Manage group information inside the executor

--- src/main/java/edu/snu/mist/core/task/DefaultQueryManagerImpl.java
+++ src/main/java/edu/snu/mist/core/task/DefaultQueryManagerImpl.java
@@ -21,8 +21,11 @@ import edu.snu.mist.core.task.stores.QueryInfoStore;
 import edu.snu.mist.formats.avro.AvroOperatorChainDag;
 import edu.snu.mist.formats.avro.QueryControlResult;
 import org.apache.reef.io.Tuple;
+import org.apache.reef.tang.Tang;
 
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ScheduledExecutorService;
@@ -68,6 +71,11 @@ final class DefaultQueryManagerImpl implements QueryManager {
   private final QueryStarter queryStarter;
 
   /**
+   * A map which contains groups and their information.
+   */
+  private final Map<String, GroupInfo> groupInfoMap;
+
+  /**
    * Default query manager in MistTask.
    * @param dagGenerator the generator that generates the logical and execution dag from avro operator chain dag.
    * @param threadManager thread manager
@@ -84,6 +92,7 @@ final class DefaultQueryManagerImpl implements QueryManager {
     this.scheduler = schedulerWrapper.getScheduler();
     this.planStore = planStore;
     this.queryStarter = queryStarter;
+    this.groupInfoMap = new HashMap<>();
   }
 
   /**
@@ -103,8 +112,17 @@ final class DefaultQueryManagerImpl implements QueryManager {
       // converts the avro operator chain dag to the logical and execution dag
       planStore.saveAvroOpChainDag(tuple);
       final LogicalAndExecutionDag logicalAndExecutionDag = dagGenerator.generate(tuple);
+      final String queryId = tuple.getKey();
       // Store the logical dag in memory
-      logicalDagMap.putIfAbsent(tuple.getKey(), logicalAndExecutionDag.getLogicalDag());
+      logicalDagMap.putIfAbsent(queryId, logicalAndExecutionDag.getLogicalDag());
+      // Update group information
+      final String groupId = tuple.getValue().getGroupId();
+      if (!groupInfoMap.containsKey(groupId)) {
+        // Add new group id, if it doesn't exist
+        groupInfoMap.put(groupId, Tang.Factory.getTang().newInjector().getInstance(GroupInfo.class));
+      }
+      // Add the query into the group
+      groupInfoMap.get(groupId).addQueryIdToGroup(queryId);
       // Start the submitted dag
       queryStarter.start(logicalAndExecutionDag.getExecutionDag());
       queryControlResult.setIsSuccess(true);
