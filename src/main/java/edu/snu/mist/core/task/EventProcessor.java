0ab1e56fc4dc533fbdab7af2ff3e423104fbdde6
DifferentSC <bert1234@snu.ac.kr>
2017-04-12 11:38:03 +0900

    Address comments

--- src/main/java/edu/snu/mist/core/task/EventProcessor.java
+++ src/main/java/edu/snu/mist/core/task/EventProcessor.java
@@ -15,6 +15,9 @@
  */
 package edu.snu.mist.core.task;
 
+import edu.snu.mist.core.parameters.PollingIntervalMilliseconds;
+import org.apache.reef.tang.annotations.Parameter;
+
 /**
  * This class processes events of queries
  * by picking up an operator chain from the OperatorChainManager.
@@ -25,35 +28,37 @@ public final class EventProcessor implements Runnable {
    * The polling interval when the thread wakes up and polls whether there is an event
    * or not.
    */
-  private final int pollingIntervalMillisecond = 100;
+  private final int pollingIntervalMillisecond;
 
   /**
    * The operator chain manager for picking up a chain for event processing.
    */
   private final OperatorChainManager operatorChainManager;
 
-  public EventProcessor(final OperatorChainManager operatorChainManager) {
-    this.operatorChainManager = operatorChainManager;
+  public EventProcessor(@Parameter(PollingIntervalMilliseconds.class) final int pollingIntervalMillisecondParam,
+                        final OperatorChainManager operatorChainManagerParam) {
+    this.pollingIntervalMillisecond = pollingIntervalMillisecondParam;
+    this.operatorChainManager = operatorChainManagerParam;
   }
 
   @Override
   public void run() {
-    while (!Thread.currentThread().isInterrupted()) {
-      try {
-        final OperatorChain query = operatorChainManager.pickOperatorChain();
-        if (query != null) {
-          query.processNextEvent();
-        } else {
-          Thread.currentThread().sleep(pollingIntervalMillisecond);
-        }
-      } catch (final Exception t) {
+    try {
+      while (!Thread.currentThread().isInterrupted()) {
         try {
+          final OperatorChain query = operatorChainManager.pickOperatorChain();
+          if (query != null) {
+            query.processNextEvent();
+          } else {
+            Thread.currentThread().sleep(pollingIntervalMillisecond);
+          }
+        } catch (final Exception t) {
           throw t;
-        } catch (final InterruptedException e) {
-          // The thread is interrupted while sleeping, then finishes its running
-          return;
         }
       }
+    } catch (final InterruptedException e) {
+      // Interrupt occurs while sleeping, so just finishes the process...
+      return;
     }
   }
 }
