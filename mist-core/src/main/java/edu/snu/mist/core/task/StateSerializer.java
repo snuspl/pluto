bf689c077b3f050bfc7e7abd54ff3ee4cc721d68
Gyewon Lee <strayyyyyy@gmail.com>
2018-07-03 15:59:57 +0900

    [MIST-1161] Fix ClassNotFoundException on state deserialization (#1164)
    This PR closes #1161 via
    
    * Adding external class loaders for deserialization.
    * Use internal `SerializeUtils` when (de)serializing the states.

--- mist-core/src/main/java/edu/snu/mist/core/task/StateSerializer.java
+++ mist-core/src/main/java/edu/snu/mist/core/task/StateSerializer.java
@@ -15,7 +15,10 @@
  */
 package edu.snu.mist.core.task;
 
-import java.io.*;
+import edu.snu.mist.common.SerializeUtils;
+
+import java.io.IOException;
+import java.io.Serializable;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
@@ -65,13 +68,8 @@ public final class StateSerializer {
    */
   private static ByteBuffer serializeState(final Object obj) throws RuntimeException {
     try {
-      try (final ByteArrayOutputStream b = new ByteArrayOutputStream()) {
-        try (final ObjectOutputStream o = new ObjectOutputStream(b)) {
-          o.writeObject(obj);
-          o.flush();
-        }
-        return ByteBuffer.wrap(b.toByteArray());
-      }
+      final String serializedString = SerializeUtils.serializeToString((Serializable) obj);
+      return ByteBuffer.wrap(serializedString.getBytes());
     } catch (final IOException e) {
       LOG.log(Level.SEVERE, "An exception occured while serializing the state.");
       e.printStackTrace();
@@ -79,19 +77,24 @@ public final class StateSerializer {
     }
   }
 
+  public static Map<String, Object> deserializeStateMap(final Map<String, Object> serializedStateMap) {
+    return deserializeStateMap(serializedStateMap, null);
+  }
+
   /**
    * Receives a Map<String, ByteBuffer>, deserializes the values, and returns it.
    * @param serializedStateMap
    * @return the deserialized StateMap
    */
-  public static Map<String, Object> deserializeStateMap(final Map<String, Object> serializedStateMap)
+  public static Map<String, Object> deserializeStateMap(final Map<String, Object> serializedStateMap,
+                                                        final ClassLoader classLoader)
       throws RuntimeException {
     final Map<String, Object> result = new HashMap<>();
     for (final Map.Entry<String, Object> mapEntry : serializedStateMap.entrySet()) {
       final Object state = mapEntry.getValue();
       if (state instanceof ByteBuffer) {
         try {
-          final Object deserializedState = deserializeState((ByteBuffer)state);
+          final Object deserializedState = deserializeState((ByteBuffer)state, classLoader);
           result.put(mapEntry.getKey(), deserializedState);
         } catch (final RuntimeException e) {
           throw e;
@@ -108,14 +111,15 @@ public final class StateSerializer {
    * @param byteBuffer
    * @return the deserialized state
    */
-  private static Object deserializeState(final ByteBuffer byteBuffer) throws RuntimeException {
+  private static Object deserializeState(final ByteBuffer byteBuffer,
+                                         final ClassLoader classLoader) throws RuntimeException {
     final byte[] bytes = new byte[byteBuffer.remaining()];
     byteBuffer.get(bytes);
     try {
-      try (final ByteArrayInputStream b = new ByteArrayInputStream(bytes)) {
-        try (final ObjectInputStream o = new ObjectInputStream(b)) {
-          return o.readObject();
-        }
+      if (classLoader != null) {
+        return SerializeUtils.deserializeFromString(new String(bytes), classLoader);
+      } else {
+        return SerializeUtils.deserializeFromString(new String(bytes));
       }
     } catch (final Exception e) {
       LOG.log(Level.SEVERE, "An exception occured while deserializing the state.");
